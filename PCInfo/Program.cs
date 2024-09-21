be899378c504969e05862ed1044eebec0300a409
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-21 07:49:43 +0330

    - Change Startup Form To Main Form

--- PCInfo/Program.cs
+++ PCInfo/Program.cs
@@ -16,7 +16,7 @@ namespace PCInfo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PCInfo.Forms.Main());
         }
     }
 }
